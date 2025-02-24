79d8fd8906bda031bf563ccd9dc739fe378cdf55
PW - your-name <duongvan5801@gmail.com>
2025-02-24 23:50:19 +0700

    tasks: homework js 2

---
+++ tests/lesson-2/ex1.js
@@ -0,0 +1,6 @@
+const number = 12;
+const name = "my number";
+const isEven = false;
+if(number % 2 === 0){
+    isEven = true;
+}
---
+++ tests/lesson-2/ex3.js
@@ -0,0 +1,6 @@
+const weight = 50;
+const height = 165;
+
+const perfectWeight = (65 * 9 )/10;
+const maxWeight = 65;
+const minWeight = (65 * 8) / 10;
